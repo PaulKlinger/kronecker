004c7141d83faee69355e52f2e09a9b3943300d1
Paul Klinger <klinger.paul@gmail.com>
2021-10-03 20:54:22 +0100

    added tests

---
+++ tests/unit/test_numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from src.kronecker import create_index_array
+
+
+def test_create_index_array():
+    expected = np.array([
+        [[0, 0], [1, 1], [2, 2], [3, 3]]
+    ] * 3)
+    res = create_index_array((3, 4, 2), 1)
+
+    np.testing.assert_array_equal(res, expected)
+
+
+def test_create_index_array_single():
+    expected = np.array([0, 1, 2, 3])
+    res = create_index_array((4,), 0)
+    np.testing.assert_array_equal(res, expected)
