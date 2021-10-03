6af4ce510430db5e332b0b6ed915a7bc9e0edd8d
Paul Klinger <klinger.paul@gmail.com>
2021-10-03 18:05:30 +0100

    common Term baseclass for Indices and Operations

--- src/kronecker.py
+++ src/kronecker.py
@@ -1,36 +1,21 @@
 from __future__ import annotations
-import enum
+import abc
 from numbers import Number
 from typing import Sequence, Tuple, Callable, Dict
 
 import numpy as np
 
 
-class Operation:
-    def __init__(self, indices: Sequence[Index], f: IndexFun):
-        self.indices = indices
-        self.f = f
-
+class Term(abc.ABC):
+    @abc.abstractmethod
     def __call__(self, values: IndexFun) -> np.ndarray:
-        return self.f(values)
+        pass
     
-    def __add__(self, other) -> Operation:
-        if isinstance(other, Index):
-            return Operation(self.indices, lambda v: self(v) + v[other])
-        elif isinstance(other, Number):
-            return Operation(self.indices, lambda v: self(v) + other)
-        elif isinstance(other, Operation):
-            return Operation(self.indices, lambda v: self(v) + other(v))
-
-        return NotImplemented
-
     def __comparison_op(self, other, operator: np.ufunc) -> Equation:
-        if isinstance(other, Operation):
+        if isinstance(other, Term):
             return Equation(self, other, operator)
-        elif isinstance(other, Index):
-            return Equation(self, Operation(self.indices, lambda v: v[other]), operator)
         elif isinstance(other, Number):
-            return Equation(self, Operation(self.indices, lambda v: other), operator)
+            return Equation(self, CompositeTerm(self.indices, lambda v: other), operator)
         
         return NotImplemented
 
@@ -51,6 +36,38 @@ class Operation:
     
     def __le__(self, other) -> Equation:
         return self.__comparison_op(other, np.less_equal)
+
+    def __add__(self, other) -> Term:
+        if isinstance(other, Number):
+            return CompositeTerm(self.indices, lambda v: self(v) + other)
+        elif isinstance(other, Term):
+            return CompositeTerm(self.indices, lambda v: self(v) + other(v))
+
+        return NotImplemented
+
+
+class Index(Term):
+    def __init__(self, n: int):
+        self.indices = None
+        self.n = n
+
+    def __hash__(self):
+        return id(self)
+
+    def __call__(self, values: IndexFun) -> np.ndarray:
+        return values[self]
+
+
+IndexFun = Callable[[Dict[Index, np.ndarray]], np.ndarray]
+
+
+class CompositeTerm(Term):
+    def __init__(self, indices: Sequence[Index], f: IndexFun):
+        self.indices = indices
+        self.f = f
+
+    def __call__(self, values: IndexFun) -> np.ndarray:
+        return self.f(values)
         
 
 def create_index_array(shape: Tuple[int,...], index_dim: int) -> np.ndarray:
@@ -61,7 +78,7 @@ def create_index_array(shape: Tuple[int,...], index_dim: int) -> np.ndarray:
 
 
 class Equation:
-    def __init__(self, left: Operation, right: Operation, operator: np.ufunc):
+    def __init__(self, left: Term, right: Term, operator: np.ufunc):
         if left.indices != right.indices:
             raise ValueError(f"Shape mismatch: {left.shape}, {right.shape}")
             
@@ -77,58 +94,6 @@ class Equation:
     def toarray(self):
         index_values = self._create_index_arrays()
         return self.operator(self.left(index_values), self.right(index_values))
-
-
-class Index:
-    def __init__(self, n: int):
-        self.indices = None
-        self.n = n
-
-    def __hash__(self):
-        return id(self)
-    
-    def __add__(self, other) -> Operation:
-        if isinstance(other, Index):
-            return Operation(self.indices, lambda v: v[self] + v[other])
-        elif isinstance(other, Number):
-            return Operation(self.indices, lambda v: v[self] + other)
-
-        return NotImplemented
-
-    def __comparison_op(self, other, operator) -> Operation:
-        if isinstance(other, Number):
-            return Equation(
-                Operation(self.indices, lambda v: v[self]),
-                Operation(self.indices, lambda v: other),
-                operator)
-        elif isinstance(other, Index):
-            return Equation(
-                Operation(self.indices, lambda v: v[self]),
-                Operation(self.indices, lambda v: v[other]),
-                operator)
-        
-        return NotImplemented
-    
-    def __eq__(self, other) -> Equation:
-        return self.__comparison_op(other, np.equal)
-    
-    def __neq__(self, other) -> Equation:
-        return self.__comparison_op(other, np.not_equal)
-    
-    def __gt__(self, other) -> Equation:
-        return self.__comparison_op(other, np.greater)
-    
-    def __ge__(self, other) -> Equation:
-        return self.__comparison_op(other, np.greater_equal)
-    
-    def __lt__(self, other) -> Equation:
-        return self.__comparison_op(other, np.less)
-    
-    def __le__(self, other) -> Equation:
-        return self.__comparison_op(other, np.less_equal)
-
-
-IndexFun = Callable[[Dict[Index, np.ndarray]], np.ndarray]
 
 
 def dims(shape: Sequence[int]) -> Tuple[Index]:
